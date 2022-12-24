d9b6e48ef196c6782eb275227220a347b6bde506
abenteuerzeit <amroz.muzyka@gmail.com>
2022-12-24 01:30:57 +0100

    Create a concrete implementation of the service interface that uses the repository pattern to perform operations on the entities

---
+++ meal-plan-generator/Services/Service.cs
@@ -0,0 +1,38 @@
+﻿using meal_plan_generator.Repository;
+
+namespace meal_plan_generator.Services
+{
+    public class Service<TEntity> : IService<TEntity> where TEntity : class
+    {
+        private readonly IRepository<TEntity> _repository;
+
+        public Service(IRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _repository.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _repository.Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _repository.Remove(entity);
+        }
+    }
