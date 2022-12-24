ff1b77a9674b12c7b9f9394ebf7930a335834d4b
abenteuerzeit <amroz.muzyka@gmail.com>
2022-12-24 01:31:17 +0100

    Implement interfaces

--- meal-plan-generator/Repository/IRepository.cs
+++ meal-plan-generator/Repository/IRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace meal_plan_generator.Repository
 {
-    public interface IRepository
+    public interface IRepository<TEntity> where TEntity : class
     {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
     }
+
 }
--- meal-plan-generator/Services/IService.cs
+++ meal-plan-generator/Services/IService.cs
@@ -1,6 +1,12 @@
 ﻿namespace meal_plan_generator.Services
 {
-    public interface IService
+    public interface IService<TEntity> where TEntity : class
     {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
     }
+
 }
