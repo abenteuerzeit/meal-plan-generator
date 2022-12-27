14e8a65efa625e6d93a3231f84c8b5908383a800
abenteuerzeit <amroz.muzyka@gmail.com>
2022-12-27 15:13:22 +0100

    Fix naming

--- meal-plan-generator/Models/MealPlan/NutrientSettings.cs
+++ meal-plan-generator/Models/MealPlan/NutrientSettings.cs
@@ -2,9 +2,9 @@
 {
     public class NutrientSettings
     {
-        public Dictionary<string, int> LessThanMax { get; set; } = useDefaults();
-        public Dictionary<string, int> MoreThanMin { get; set; } = useDefaults();
-        public Dictionary<string, int> LessThanMin { get; set; } = useDefaults();
+        public Dictionary<string, int> LessThanMax { get; set; } = UseDefaults();
+        public Dictionary<string, int> MoreThanMin { get; set; } = UseDefaults();
+        public Dictionary<string, int> LessThanMin { get; set; } = UseDefaults();
 
 
         NutrientSettings(Dictionary<string, int> lessThanMax, Dictionary<string, int> moreThanMin, Dictionary<string, int> lessThanMin)
@@ -15,7 +15,7 @@
             LessThanMin = lessThanMin;
         }
 
-        private static Dictionary<string, int> useDefaults()
+        private static Dictionary<string, int> UseDefaults()
         {
             return new Dictionary<string, int>()
                 {
