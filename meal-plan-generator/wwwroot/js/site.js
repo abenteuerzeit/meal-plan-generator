00f228358e33dcf52c24007fd36288f212cfc8d7
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-05 16:27:51 +0100

    fill out the form data with random values

--- meal-plan-generator/wwwroot/js/site.js
+++ meal-plan-generator/wwwroot/js/site.js
@@ -2,6 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function setFormRandomValues() {
+    // Generate a random date between 1900 and today
+    const min = new Date(1900, 0, 1).getTime();
+    const max = new Date().getTime();
+    const randomTimestamp = Math.floor(Math.random() * (max - min)) + min;
+    const randomDate = new Date(randomTimestamp);
+    const formattedRandomDate = randomDate.toISOString().slice(0, 10);
+
+    // Set the random date as the value of the birthdate input
+    const birthdateInput = document.getElementById("birthdate");
+    birthdateInput.value = formattedRandomDate;
+
+    // Select a random option from the gender select element
+    const genderSelect = document.getElementById("gender");
+    const randomIndex = Math.floor(Math.random() * genderSelect.options.length);
+    genderSelect.options[randomIndex].selected = true;
+
+    // Set random values for the nutrient data inputs
+    const nutrientDataInputs = document.querySelectorAll(".form__input");
+    nutrientDataInputs.forEach(input => {
+        const randomValue = Math.random() * 100;
+        input.value = randomValue;
+    });
+}
+
+
+
+
 function calculateAge(birthday) {
     var ageDifMs = Date.now() - birthday.getTime();
     var ageDate = new Date(ageDifMs);
